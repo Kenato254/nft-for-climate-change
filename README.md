d8f153fd60d284f084e90226ffa3a5ddb7a04efc
dapman601 <pohema1987@gmail.com>
2022-12-08 12:17:30 +0100

    submission

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is in fulfillment of the ABC blockchain bootcamp final project. This is a NFT project to support planting of trees in climate change.
---
+++ scripts/mint-nft.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.providers.AlchemyProvider('goerli', API_KEY)
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = '0x941a7947829d8f82244296c87c57c92c553000e5'
+
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmWer5Nhgy35oHPP3hmU962N6PKepXUSH3nSiA26LiyZv8"
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+    await nftTxn.wait()
+    console.log(`NFT Minted! Check it out at: https://goerli.etherscan.io/tx/${nftTxn.hash}`)
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
